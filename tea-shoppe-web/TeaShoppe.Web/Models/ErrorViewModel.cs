8cdf5aa6b0297eaf019372e74326dabe1e79f4a1
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-03-13 16:21:50 -0400

    Add initial ASP.NET Core MVC project for assignment 3

---
+++ tea-shoppe-web/TeaShoppe.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TeaShoppe.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
