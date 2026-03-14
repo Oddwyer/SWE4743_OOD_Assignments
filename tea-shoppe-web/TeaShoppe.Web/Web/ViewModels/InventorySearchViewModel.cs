fabbebb65b51d18ee7aa3fae6eb0ff9ff3e5a29a
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-03-14 16:22:41 -0400

    InventorySearchViewModel created

---
+++ tea-shoppe-web/TeaShoppe.Web/Web/ViewModels/InventorySearchViewModel.cs
@@ -0,0 +1,28 @@
+using TeaShoppe.Web.Domain.Common;
+using TeaShoppe.Web.Domain.Inventory;
+
+namespace TeaShoppe.Web.Web.ViewModels;
+
+/// <summary>
+/// Mirroring data fields in QueryItem (servicing backend domain logic) this model class services the
+/// frontend by collecting necessary data to pass to the backend for creating QueryItems, performing
+/// queries, and returning query results.
+/// </summary>
+public class InventorySearchViewModel
+{
+    // Search inputs.
+    public string? SearchName { get; set; }
+    public int MinRating { get; set; } = 3;
+    public int MaxRating { get; set; } = 5;
+    public int? Quantity { get; set; } = 0;
+    public decimal MinPrice { get; set; } = 0.00m;
+    public decimal MaxPrice { get; set; } = 1000.00m;
+    public PrimarySort Sort { get; set; } = PrimarySort.Price;
+    public SortDirection PriceDirection { get; set; } = SortDirection.Ascending;
+    public SortDirection RatingDirection { get; set; } = SortDirection.Descending;
+    public bool? IsInStock { get; set; }
+
+    // Search outputs.
+    public IReadOnlyList<InventoryItem> Items { get; set; } = new List<InventoryItem>();
+    public string AppliedFilters { get; set; } = string.Empty;
+}
