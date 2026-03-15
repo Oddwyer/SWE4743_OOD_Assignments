734f938f8e2335a7fdff11160dc2b270422ada66
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-03-15 14:42:53 -0400

    Update Search View to map GUID for CheckOutService logic.

--- tea-shoppe-web/TeaShoppe.Web/Application/Services/CheckoutService.cs
+++ tea-shoppe-web/TeaShoppe.Web/Application/Services/CheckoutService.cs
@@ -1,20 +1,25 @@
 using TeaShoppe.Web.Application.Interfaces;
 using TeaShoppe.Web.Domain.Orders;
 using TeaShoppe.Web.Application.Factories;
+using TeaShoppe.Web.Domain.Inventory;
+using TeaShoppe.Web.Domain.InventoryQuery;
 
 namespace TeaShoppe.Web.Application.Services;
-
+/*
 public class CheckoutService
 {
     PaymentStrategyFactory _paymentStrategyFactory;
+    private IReadOnlyList<InventoryItem>  _repository;
 
-    public CheckoutService(PaymentStrategyFactory paymentStrategyFactory)
+    public CheckoutService(PaymentStrategyFactory paymentStrategyFactory, IInventory repository)
     {
         _paymentStrategyFactory = paymentStrategyFactory;
+        _repository = repository.GetInventory();
     }
 
-    private bool ValidateQuantity(OrderItem item)
+    private bool ValidateQuantity(QueryItem item)
     {
-        
+        selectedItem = _repository.FirstOrDefault(i => i.InventoryItemId == item.)
+        if(_repository.Contains(item.OrderItemId)
     }
-}
+}*/
