ee859194b8467a75053b1a535df36b2e676a48c1
Oddwyer <233861108+Oddwyer@users.noreply.github.com>
2026-03-15 14:03:34 -0400

    Added Order, OrderItem classes.

---
+++ tea-shoppe-web/TeaShoppe.Web/Domain/Orders/Order.cs
@@ -0,0 +1,131 @@
+namespace TeaShoppe.Web.Domain.Orders;
+
+/// <summary>
+/// Class for customer order; holds multiple order items.
+/// </summary>
+
+public class Order
+{
+    private readonly List<OrderItem> _orderItems = new();
+
+    // Constructor to initializes order. 
+    public Order()
+    {
+    }
+
+    // Constructor to begin order.
+    public Order(OrderItem item, int qty)
+    {
+        item.IncrementQuantity(qty);
+        _orderItems.Add(item);
+    }
+
+    // Add items to order.
+    public bool AddItem(OrderItem item, int qty)
+    {
+        OrderItem? existing = SearchOrder(item.OrderItemId);
+        if (existing != null)
+        {
+            int before = existing.Quantity;
+            existing.IncrementQuantity(qty);
+            return existing.Quantity == before + qty;
+        }
+
+        item.IncrementQuantity(qty);
+        _orderItems.Add(item);
+        return _orderItems.Contains(item);
+    }
+
+    // Remove items from order.
+    public bool RemoveItem(OrderItem item, int qty)
+    {
+        OrderItem? existing = SearchOrder(item.OrderItemId);
+        int? count = existing.Quantity - qty;
+        if (existing != null)
+        {
+            int before = existing.Quantity;
+            if (existing.Quantity > qty)
+            {
+                existing.DecrementQuantity(qty);
+                return existing.Quantity == before - qty;
+            }
+
+            _orderItems.Remove(existing);
+            return !_orderItems.Contains(existing);
+        }
+        return false;
+    }
+
+    // Return order total cost. 
+    public decimal OrderTotal()
+    {
+        decimal total = 0.00m;
+        foreach (OrderItem x in _orderItems)
+        {
+            total += x.Price * x.Quantity;
+        }
+
+        return total;
+    }
+    
+    // Return count of all items in order.
+
+    public int TotalItemCount()
+    {
+        return _orderItems.Sum(x => x.Quantity);
+    }
+
+    
+    // Return whether order is empty.
+    public bool IsEmpty()
+    {
+        return _orderItems.Count == 0;
+    }
+
+    // Return number of line items on order. This number may differ from total count (note: number of items per line).
+    public int NumberOfLineItems()
+    {
+        return _orderItems.Count;
+    }
+
+    // Return order details.
+    public string OrderDetails()
+    {
+        if (!IsEmpty())
+        {
+            string details = "\nCurrent Order:\n";
+            for (int i = 0; i < _orderItems.Count; i++)
+            {
+                details += $"{i+1}: {_orderItems[i].Name} - Quantity: {_orderItems[i].Quantity}\n";
+            }
+
+            details += $"Total Items: {TotalItemCount()}\n";
+            details += $"Order Total: ${OrderTotal(): 0.00}\n";
+            return details;
+        }
+        else
+        {
+            return "Your order is currently empty.";
+        }
+    }
+
+    // Search order for specific item.
+    public OrderItem? SearchOrder(Guid itemId)
+    {
+        foreach (OrderItem x in _orderItems)
+        {
+            if (x.OrderItemId == itemId)
+            {
+                return x;
+            }
+        }
+
+        return null;
+    }
+
+    // Return item in order.
+    public OrderItem GetItem(int index)
+    {
+        return  _orderItems[index];
+    }
+}
---
+++ tea-shoppe-web/TeaShoppe.Web/Domain/Orders/OrderItem.cs
@@ -0,0 +1,41 @@
+using TeaShoppe.Web.Domain.Inventory;
+
+namespace TeaShoppe.Web.Domain.Orders;
+
+/// <summary>
+/// Class representing each order item in customer order.
+/// </summary>
+
+public class OrderItem
+{
+    public string Name {get;}
+    public Guid OrderItemId { get; }
+    public int Quantity { get; private set; } = 0;
+    public decimal Price { get; }
+
+    public OrderItem(InventoryItem item, int quantity)
+    {
+        Name = item.Name;
+        OrderItemId = item.InventoryItemId;
+        Price = item.Price;
+        Quantity = quantity;
+    }
+
+    // Increment quantity of order item.
+    public void IncrementQuantity(int qty)
+    {
+        Quantity += qty;
+    }
+
+    // Decrement quantity of inventory item.
+    public void DecrementQuantity(int qty)
+    {
+        if (Quantity - qty < 0)
+        {
+            throw new InvalidOperationException("Quantity cannot go below zero.");
+        }
+        else{
+            Quantity -= qty;
+        }
+    }
+}
